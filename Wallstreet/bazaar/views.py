02ecd9d498720c329af83696c059dd5f0bc269f1
Kunal Chadha <kunalchadhaks2412@gmail.com>
2019-06-16 12:22:54 +0530

    ...

--- Wallstreet/bazaar/views.py
+++ Wallstreet/bazaar/views.py
@@ -22,14 +22,17 @@ class Register(View):
 
     def post(self, request):
         try:
-            user = User.objects.create_user(username=request.POST["username"])
-            password=User.objects.make_random_password(length=6)
-            user.set_password(password)
-            user.save()
+            g = Global.objects.get(pk=1)
+            if request.POST["password"] == g.registrationKey:
+                user = User.objects.create_user(username=request.POST["username"])
+                password = User.objects.make_random_password(length=6)
+                user.set_password(password)
+                user.save()
 
-            profile = Profile.objects.create(user=user)
-            profile.save()
-            return render(request, self.template, {"pass":password})
+                profile = Profile.objects.create(user=user)
+                profile.save()
+                return render(request, self.template, {"pass": password})
+            return render(request, self.template, {"error": "Invalid Registration"})
         except IntegrityError:
             return render(request, self.template, {"error": "Invalid Registration"})
 
@@ -51,12 +54,10 @@ class Login(View):
                 login(request, user)
                 return redirect("../")
         else:
-            return render(request, self.template, {})
-
+            return render(request, self.template, {"error": "Invalid Username or Password"})
 
 
 def Logoff(request):
-    template = 'bazaar/login.html'
     if request.user.is_authenticated:
         logout(request)
     return redirect("../login")
@@ -71,7 +72,7 @@ class postlogin(View):
         all_companies = Company.objects.all()
         profile = Profile.objects.filter(user=User.objects.get(username=request.user)).first()
         shares = UserShareTable.objects.filter(profile=profile)
-        context = {"companies": all_companies, "profile": profile,"userShareTable":shares}
+        context = {"companies": all_companies, "profile": profile, "userShareTable": shares}
         return render(request, self.template, context)
 
 
